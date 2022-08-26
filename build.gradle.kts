6a1764da161a6707a932e4f1933c8c51051cab1a
gamedec <122694048+gamedec@users.noreply.github.com>
2022-08-26 07:06:13 +0000

    Upgrade typescript

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,6 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    kotlin("jvm") version "1.4.10"
+    id("com.github.johnrengelman.shadow") version "6.0.0"
+}
