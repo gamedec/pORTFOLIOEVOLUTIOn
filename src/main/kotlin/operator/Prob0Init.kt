e9db34a5e4a3518fe932b54ab416b7cddcabcbbc
gamedec <122694048+gamedec@users.noreply.github.com>
2023-01-18 13:22:37 +0000

    Update form.md

--- src/main/kotlin/operator/Prob0Init.kt
+++ src/main/kotlin/operator/Prob0Init.kt
@@ -6,3 +6,8 @@ import kotlin.math.pow
 /**
  * Created by DEDZTBH on 2020/09/15.
  * Project CMU_Coin-flipping_Experience
+ */
+
+fun allStates(n: Int) =
+    Array(2.0.pow(n).toInt()) {
+        IntArray(n) { i -> (it shr i) and 1 }.apply { reverse() }
