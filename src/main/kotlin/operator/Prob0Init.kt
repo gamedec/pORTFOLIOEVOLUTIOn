9d384ec42e1c263903abd980d07400182bd11cbc
gamedec <122694048+gamedec@users.noreply.github.com>
2023-02-03 08:25:06 +0000

    Update DataHarmonization-table.md

--- src/main/kotlin/operator/Prob0Init.kt
+++ src/main/kotlin/operator/Prob0Init.kt
@@ -19,3 +19,6 @@ class Prob0Init(N: Int) : ProbFinder(N) {
         allStates(N).forEach { endState ->
             var prob = 1.0
             endState.forEach { i ->
+                val x = probs[i]
+                prob *= if (i > 0) x else 1.0 - x
+            }
