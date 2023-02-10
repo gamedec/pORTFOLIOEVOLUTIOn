efc9d923e1f50a71e5c85af9622d4b5734dadec8
gamedec <122694048+gamedec@users.noreply.github.com>
2023-02-10 21:48:31 +0000

    new flies

--- src/main/kotlin/operator/ProbFinder.kt
+++ src/main/kotlin/operator/ProbFinder.kt
@@ -3,3 +3,8 @@ package operator
 
 import org.ejml.data.DMatrixRMaj
 import org.ejml.dense.row.CommonOps_DDRM
+import org.ejml.kotlin.minus
+import org.ejml.kotlin.plus
+import org.ejml.kotlin.plusAssign
+import org.ejml.kotlin.times
+import util.*
