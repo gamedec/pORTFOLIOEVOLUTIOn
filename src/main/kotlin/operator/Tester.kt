54eed52fe7559187152f4c9d7b8d32d221d6ceb0
gamedec <122694048+gamedec@users.noreply.github.com>
2023-03-12 14:32:04 +0000

    Fix issue #10

--- src/main/kotlin/operator/Tester.kt
+++ src/main/kotlin/operator/Tester.kt
@@ -9,3 +9,6 @@ import kotlin.random.Random
  * Created by DEDZTBH on 2020/09/15.
  * Project CMU_Coin-flipping_Experience
  */
+
+infix fun IntArray.flip(i: Int) = set(i, get(i) xor 1)
+fun IntArray.swap(i: Int, j: Int) = set(i, get(j).also { set(j, get(i)) })
