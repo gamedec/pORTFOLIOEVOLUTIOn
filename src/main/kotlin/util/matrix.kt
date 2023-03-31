f5ba14b9d1b2b44cbf1f4430d9bae39477d9466f
gamedec <122694048+gamedec@users.noreply.github.com>
2023-03-31 11:27:31 +0000

    Fix the comment

--- src/main/kotlin/util/matrix.kt
+++ src/main/kotlin/util/matrix.kt
@@ -4,3 +4,8 @@ package util
 import org.ejml.data.DMatrixRMaj
 import org.ejml.dense.row.CommonOps_DDRM
 import org.ejml.simple.ops.SimpleOperations_DDRM
+
+/**
+ * Created by DEDZTBH on 2020/09/18.
+ * Project CMU_Coin-flipping_Experience
+ */
